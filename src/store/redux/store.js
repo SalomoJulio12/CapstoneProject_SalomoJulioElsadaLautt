323fe6ecaf2047a122dbf5e286c9be5184e5a6f3
SalomoJulio12 <salomojulio0@mhs.eng.upr.ac.id>
2024-12-20 12:45:46 +0700

    Updated 1x

--- src/store/redux/store.js
+++ src/store/redux/store.js
@@ -1,8 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducer';
+import { configureStore } from '@reduxjs/toolkit'; // Mengimpor fungsi configureStore dari Redux Toolkit untuk konfigurasi store
+import rootReducer from './reducer'; // Mengimpor rootReducer, yang menggabungkan semua reducer di aplikasi
 
+// Membuat store Redux dengan configureStore yang disediakan oleh Redux Toolkit
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: rootReducer, // rootReducer adalah fungsi reducer utama yang menggabungkan semua slice reducer
 });
 
 export default store;
